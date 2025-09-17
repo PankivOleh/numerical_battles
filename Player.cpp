e99d9a9ee0802322088a7803fdb5bbc9907a8a1d
Oleg <oleg2812POA2006@gmail.com>
2025-09-18 01:12:55 +0300

    Start

---
+++ Player.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Oleg on 18.09.2025.
+//
+
+#include "Player.h"
+
+Clas
+
---
+++ Player.h
@@ -0,0 +1,14 @@
+//
+// Created by Oleg on 18.09.2025.
+//
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+class Player{
+
+};
+
+
+
+#endif //PLAYER_H
