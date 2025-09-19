9887eeea7149b050e61f4d8f4aac676e3000eec1
Admin <oleg2812POA2006@gmail.com>
2025-09-19 20:15:18 +0300

    Опис класу гравця , визначення гетерів і сетерів.

--- Player.cpp
+++ Player.cpp
@@ -3,6 +3,66 @@
 //
 
 #include "Player.h"
+#include <iostream>
 
-Clas
+using namespace std;
 
+
+
+    Player::Player(string name, int maxhp, int hp, int level, int difficult) {
+        this->name = name ;
+        this ->hp = hp ;
+        this -> level = level ;
+        this -> difficult = difficult ;
+        this -> maxhp = maxhp/difficult ;
+
+
+    }
+    int Player:: get_hp() {
+        return hp ;
+    }
+    int Player:: get_difficult() {
+        return difficult ;
+    }
+    int Player:: get_level() {
+        return level ;
+    }
+    int Player::set_hp(int hp) {
+        if (this->hp == maxhp) {
+            cout << "Player already has max hp" << endl ;
+            return 0 ;
+        }
+        if (hp+this->hp > maxhp) {
+            this->hp = maxhp ;
+            return 0 ;
+        }
+        this -> hp = hp+this->hp;
+        if(this->hp <= 0 ) {
+            cout << "You lose :(" << endl ;
+            return -1 ;
+        }
+        return 0 ;
+    }
+    int Player::set_dificult(int difficult) {\
+        if(difficult>3 || difficult<1) {
+            return 1 ;
+        }
+        this -> difficult = difficult ;
+        maxhp = maxhp/difficult;
+        if(maxhp < hp) {
+            hp = maxhp ;
+        }
+        return 0 ;
+    }
+    int Player::set_level(int level) {
+        if(level<1) {
+            return 1 ;
+        }
+        if(level>5*difficult) {
+            cout<<"You won!";
+            return -1;
+        }
+        this -> level = level ;
+        return 0 ;
+    }
+    Player:: ~Player(){}
--- Player.h
+++ Player.h
@@ -4,11 +4,25 @@
 
 #ifndef PLAYER_H
 #define PLAYER_H
+#include <string>
 
-class Player{
-
+class Player {
+private:
+    std::string name;
+    int hp =1 ;
+    int level =1;
+    int difficult =1;
+    int maxhp =1;
+public:
+    Player(std::string name, int maxhp, int hp, int level, int difficult);
+    int get_hp();
+    int get_difficult();
+    int get_level();
+    int set_hp(int hp);
+    int set_dificult(int difficult);
+    int set_level(int level);
+    ~Player();
 };
-
 
 
 #endif //PLAYER_H
