2d6ae23b5e9212743235cf21455a9e80e0802463
Oleg <oleg2812POA2006@gmail.com>
2025-10-13 00:59:21 +0300

    Add Python and C++ projects

---
+++ Numerical_battles_cpp/cmake-build-debug/CMakeFiles/PyAPI_py.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Oleg/Desktop/vs codes/Numerical_Battles/Numerical_battles_cpp/numerical_battles/Card.cpp" "CMakeFiles/PyAPI_py.dir/numerical_battles/Card.cpp.obj" "gcc" "CMakeFiles/PyAPI_py.dir/numerical_battles/Card.cpp.obj.d"
+  "C:/Users/Oleg/Desktop/vs codes/Numerical_Battles/Numerical_battles_cpp/numerical_battles/Enemy.cpp" "CMakeFiles/PyAPI_py.dir/numerical_battles/Enemy.cpp.obj" "gcc" "CMakeFiles/PyAPI_py.dir/numerical_battles/Enemy.cpp.obj.d"
+  "C:/Users/Oleg/Desktop/vs codes/Numerical_Battles/Numerical_battles_cpp/numerical_battles/Game.cpp" "CMakeFiles/PyAPI_py.dir/numerical_battles/Game.cpp.obj" "gcc" "CMakeFiles/PyAPI_py.dir/numerical_battles/Game.cpp.obj.d"
+  "C:/Users/Oleg/Desktop/vs codes/Numerical_Battles/Numerical_battles_cpp/numerical_battles/Hand.cpp" "CMakeFiles/PyAPI_py.dir/numerical_battles/Hand.cpp.obj" "gcc" "CMakeFiles/PyAPI_py.dir/numerical_battles/Hand.cpp.obj.d"
+  "C:/Users/Oleg/Desktop/vs codes/Numerical_Battles/Numerical_battles_cpp/numerical_battles/Player.cpp" "CMakeFiles/PyAPI_py.dir/numerical_battles/Player.cpp.obj" "gcc" "CMakeFiles/PyAPI_py.dir/numerical_battles/Player.cpp.obj.d"
+  "C:/Users/Oleg/Desktop/vs codes/Numerical_Battles/Numerical_battles_cpp/numerical_battles/PyAPI.cpp" "CMakeFiles/PyAPI_py.dir/numerical_battles/PyAPI.cpp.obj" "gcc" "CMakeFiles/PyAPI_py.dir/numerical_battles/PyAPI.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Numerical_battles_cpp/cmake-build-debug/CMakeFiles/PyAPI_py.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PyAPI_py.dir/numerical_battles/Card.cpp.obj"
+  "CMakeFiles/PyAPI_py.dir/numerical_battles/Card.cpp.obj.d"
+  "CMakeFiles/PyAPI_py.dir/numerical_battles/Enemy.cpp.obj"
+  "CMakeFiles/PyAPI_py.dir/numerical_battles/Enemy.cpp.obj.d"
+  "CMakeFiles/PyAPI_py.dir/numerical_battles/Game.cpp.obj"
+  "CMakeFiles/PyAPI_py.dir/numerical_battles/Game.cpp.obj.d"
+  "CMakeFiles/PyAPI_py.dir/numerical_battles/Hand.cpp.obj"
+  "CMakeFiles/PyAPI_py.dir/numerical_battles/Hand.cpp.obj.d"
+  "CMakeFiles/PyAPI_py.dir/numerical_battles/Player.cpp.obj"
+  "CMakeFiles/PyAPI_py.dir/numerical_battles/Player.cpp.obj.d"
+  "CMakeFiles/PyAPI_py.dir/numerical_battles/PyAPI.cpp.obj"
+  "CMakeFiles/PyAPI_py.dir/numerical_battles/PyAPI.cpp.obj.d"
+  "PyAPI_py.cp313-win_amd64.pyd"
+  "PyAPI_py.cp313-win_amd64.pyd.manifest"
+  "PyAPI_py.pdb"
+  "libPyAPI_py.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PyAPI_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
